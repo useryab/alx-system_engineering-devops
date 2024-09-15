7b207b18d48c0e5c29275ece2be3b6ec29e09b95
Yabsira Tekalegn <yabsiratekalegn53@gmail.com>
2024-09-15 00:39:44 -0800

    error fix for subs and top_ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
-"""
+""" returns number of subscribers of reddi api """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    """ return number of subscribers of subredit """
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    r = requests.get(url, headers={'User-agent': 'norman'},
+                     allow_redirects=False)
+    if r.status_code == 200:
+        return r.json().get("data").get("subscribers")
     else:
         return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""
-Function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit.
-"""
+""" returns top ten titles of given subreddit """
 
 import requests
 
 
 def top_ten(subreddit):
-    """
-    Function that queries the Reddit API
-    - If not a valid subreddit, print None.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-        params={"limit": 10},
-    )
-
-    if req.status_code == 200:
-        for get_data in req.json().get("data").get("children"):
-            dat = get_data.get("data")
-            title = dat.get("title")
-            print(title)
+    """ print top ten post of a subreddit """
+    url = "https://www.reddit.com/r/" + subreddit + "/top/.json"
+    r = requests.get(url,
+                     headers={'User-agent': 'norman'},
+                     params={"limit": 10},
+                     allow_redirects=False)
+    if r.status_code == 200:
+        for dic in r.json().get('data').get('children'):
+            print(dic.get('data').get('title'))
     else:
-        print(None)
+        print("None")
